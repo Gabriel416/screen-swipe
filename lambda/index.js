a58ac2c8526493c391188751474e7d77c1b2acf0
Gabriel <gabriel@Gabriels-MacBook-Pro.local>
2018-12-23 12:43:22 -0500

    initial commit

---
+++ lambda/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// module.exports.hello = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Go Serverless v1.0! Your function executed successfully!",
+//       input: event
+//     })
+//   };
+
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+// };
+
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/capture-image", (req, res) => {
+  const { url } = req.body;
+});
+
+module.exports.handler = serverless(app);
